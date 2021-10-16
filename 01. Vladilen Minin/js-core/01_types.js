917066768d219c0054851eef9d2f9c65266f7cb8
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-16 23:08:02 +0500

    Data Types

---
+++ 01. Vladilen Minin/js-core/01_types.js
@@ -0,0 +1,10 @@
+// Data Types in JS are: [null, undefined, boolean, number, string, object, sybol]
+
+console.log(typeof 0);
+console.log(typeof "This is String");
+console.log(typeof `This is String`);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof Math);
+console.log(typeof null);
+console.log(typeof (12 > 11));
